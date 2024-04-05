8ffbd1543bc34766a0e5ff8909c1cf860e38fb44
Clinton Chieng <chieng.clinton21@gmail.com>
2024-04-05 14:03:46 -0700

    Added notes

--- notes-2-2-morefunctions.py
+++ notes-2-2-morefunctions.py
@@ -2,3 +2,26 @@
 # Author: Clinton
 # April 4 2024
 
+def stars(num_stars: int) -> str:
+    """Returns a fiven number of *"""
+    value = ""
+   
+    #if value == 0 or value ==1 "*"
+    # if value > 1 "*" * num_stars
+    # else negative number -> error
+    if num_stars == 0 or num_stars == 1:
+            value = "*"
+    elif num_stars
+    return "*" * num_stars
+
+
+
+# # Multiply Strings
+# Greeting = "hello"
+# print(greeting * 2)
+
+# print("The quick brown fox jumps over the lazy dog.")
+print(stars(1))    # "*"
+print(stars(100))
+print(stars(0))
+print(stars(0))
---
+++ notes-2-3-loops.py
@@ -0,0 +1,3 @@
+# Loops and Iteration
+# Author: Clinton
+# 5 April 2024
